0f54f4c331e334eba8c8cae8ca75510106243347
akshayj-testgit <aujadhav@yahoo.co.in>
2018-04-10 18:34:53 +0530

    added code

--- src/main/java/calculator/one/SimpleCalc.java
+++ src/main/java/calculator/one/SimpleCalc.java
@@ -3,32 +3,35 @@ package calculator.one;
 public class SimpleCalc {
 	
 	
-	public static void sum(int a, int b)
+	public static int sum(int a, int b)
 	{
 	int sumA= a+b;
-	System.out.println("Sum of a and b is : "+ sumA);
+	//System.out.println("Sum of a and b is : "+ sumA);
+	return sumA;
 	}
 	
 
-	public static void divide(int a, int b)
+	public static int divide(int a, int b)
 	{
 	int divideA= a/b;
-	System.out.println("divide of a and b is : "+ divideA);
+	//System.out.println("divide of a and b is : "+ divideA);
+	return divideA;
 	}
 	
 
-	public static void multiply(int a, int b)
+	public static int multiply(int a, int b)
 	{
 	int multiplyA= a*b;
-	System.out.println("multiply of a and b is : "+ multiplyA);
+	//System.out.println("multiply of a and b is : "+ multiplyA);
+	return multiplyA;
 	}
 	
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		
 		sum(10,8);
 		divide(22, 2);
 		multiply(14,5);
-	}
+	}*/
 	
 }
