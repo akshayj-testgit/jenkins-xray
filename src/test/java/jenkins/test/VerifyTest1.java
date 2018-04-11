506c0e4b72681542fcf79f59275cb78467521d0d
akshayj-testgit <aujadhav@yahoo.co.in>
2018-04-11 17:20:07 +0530

    added testng logic

--- src/test/java/jenkins/test/VerifyTest1.java
+++ src/test/java/jenkins/test/VerifyTest1.java
@@ -3,18 +3,27 @@ import calculator.one.*;
 
 //import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import com.xpandit.xray.*;
+import com.xpandit.xray.util.*;
+
 
 public class VerifyTest1 {
 
 	//SimpleCalc sCalc = new SimpleCalc();
-	
+	//XRAYAJ
 	@Test
-	public static void Addition()
+		public static void Addition()
 	{
 		//SimpleCalc sCalc = new SimpleCalc();
 		int addition= SimpleCalc.sum(10, 5);
 		System.out.println("Sum of a and b is : "+ addition);
-	
+		 ITestResult result = Reporter.getCurrentTestResult();    
+	        //result.setAttribute("requirement", "CALC-1234");
+	        result.setAttribute("test", "XRAYAJ-3");
+	       // result.setAttribute("labels", "core addition");
 	}
 	
 	@Test
@@ -24,15 +33,22 @@ public class VerifyTest1 {
 		//SimpleCalc sCalcD = new SimpleCalc();
 		int divide= SimpleCalc.divide(10, 5);
 		System.out.println("divide of a and b is : "+ divide);
+		 ITestResult result = Reporter.getCurrentTestResult();    
+	        //result.setAttribute("requirement", "CALC-1234");
+	        result.setAttribute("test", "XRAYAJ-2");
 	}
 	
 	@Test
+	//@Xray (test ="test1")
 	public static void Multiplication()
 	{
 	
 		//SimpleCalc sCalcM = new SimpleCalc();
 		int multiplication= SimpleCalc.multiply(10, 5);
 	System.out.println("multiply of a and b is : " +multiplication);
+	 ITestResult result = Reporter.getCurrentTestResult();    
+     //result.setAttribute("requirement", "CALC-1234");
+     result.setAttribute("test", "XRAYAJ-1");
 	} 
 	
 }
